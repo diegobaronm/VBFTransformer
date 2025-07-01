2be07db4ae64d1176c5b38af8e5849f4b328fea0
Diego Baron <diegobaroncharm@gmail.com>
2025-07-01 22:25:35 +0100

    Adding Hydra functionality.

--- VBFTransformer.py
+++ VBFTransformer.py
@@ -8,21 +8,27 @@ import torchmetrics
 
 import lightning as L
 
+# Hydra - for CLI configuration management
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
 # Import local modules
 from Train import train
 from Predict import predict
 from Performance import testing
 from VBFTransformerDataModule import VBFTransformerDataModule
 
-def main(mode):
-    
-    datamodule = VBFTransformerDataModule('../../data/signal_polars.csv','../../data/background_polars.csv', n_particles=7)
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))
 
-    if mode == 'train':
+    datamodule = VBFTransformerDataModule(cfg.dataset.signal_path, cfg.dataset.background_path, n_particles=7)
+
+    if cfg.opts.mode == 'train':
         # Train the model
         train(datamodule)
 
-    if mode == 'predict':
+    if cfg.opts.mode == 'predict':
        # Predict using the model
        predictions = predict(datamodule)
 
@@ -38,16 +44,10 @@ def main(mode):
            'labels': save_labels    
        })
        df.to_csv('predictions.csv', index=False)
-    
-    if mode == 'performance':
+
+    if cfg.opts.mode == 'performance':
         testing(datamodule)
 
 if __name__ == "__main__":
-    # This is the main entry point for the application.
-    import argparse
-    parser = argparse.ArgumentParser(description="Train a VBFTransformer model.")
-    parser.add_argument('--mode', help='Mode to run the script in', choices=['train', 'predict','performance'], default='train')
-    args = parser.parse_args()
-
     # Run
-    main(args.mode)
+    main()

719759772351c480f1f5c4c6aa48a00229fef082
Federico Saitta <federico.saitta2003@gmail.com>
2025-09-16 11:28:51 +0100

    Update TransformerModel.py

--- src/models/TransformerModel.py
+++ src/models/TransformerModel.py
@@ -249,5 +249,5 @@ class VBFTransformer(L.LightningModule):
         self.feature_importance['nominal'].reset()
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.model.parameters(), lr=self.learning_rate, weight_decay=0.0001, amsgrad=True)
+        optimizer = optim.AdamW(self.model.parameters(), lr=self.learning_rate, weight_decay=0.01, amsgrad=True)
         return optimizer
